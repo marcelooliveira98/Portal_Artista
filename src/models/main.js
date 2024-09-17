37d4bbdd6e96175b15e017a30eb97edfc44c0196
Marcelo Leonardo <marcelo.leonardo@academico.ifpb.edu.br>
2024-09-17 14:18:06 -0300

    Update main.js

--- src/models/main.js
+++ src/models/main.js
@@ -1,1 +1,21 @@
+import { inserirConta, inserirLogin, inserirFeedback } from './insercao.js';
 
+async function executarInserts() {
+  // Substitua os valores abaixo pelos dados reais que você quer inserir
+
+  // Inserir uma nova conta
+  await inserirConta(
+    'cpf_aqui', 'nome_aqui', 'email_aqui', 'area_aqui',
+    'biografia_aqui', 'link_aqui', 'senha_aqui',
+    'dia_aqui', 'mes_aqui', 'ano_aqui', 'genero_aqui', 'editais_aqui'
+  );
+
+  // Inserir um login
+  await inserirLogin('email_aqui', 'senha_aqui');
+
+  // Inserir um feedback
+  await inserirFeedback('projeto_aqui', 'artista_aqui', 'comentario_aqui');
+}
+
+// Executar as inserções
+executarInserts();
