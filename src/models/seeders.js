3f4be6ad9a65fb008bf7dfdc8990c5582c877046
Jacyel-Pablo <110336377+Jacyel-Pablo@users.noreply.github.com>
2024-09-19 17:40:37 -0300

    Add files via upload
    Alteração feita nas tabelas do seeders.js para ficar mas parecido com a proposta original

--- src/models/seeders.js
+++ src/models/seeders.js
@@ -3,44 +3,153 @@ import connect from "./migration.js";
 async function up() {
   const banco = await connect()
 
-  // Criar o cadastro das contas
+  // Faz o Criar as informações do usúario
+
   let sql = `
-    CREATE TABLE criar_contas (
-      cpf VARCHA(255) PRIMARY KEY,
-      nome VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      area VARCHAR(255) NOT NULL,
-      biografia VARCHAR(255) NOT NULL,
-      link VARCHAR(255) NOT NULL,
-      senha VARCHAR(255)NOT NULL,
-      dia VARCHAR(255) NOT NULL,
-      mes VARCHAR(255) NOT NULL,
-      ano VARCHAR(255) NOT NULL,
-      genero VARCHAR(255) NOT NULL,
-      editais VARCHAR(255) NOT NULL
-    )
-  `
-
-  await banco.run(sql);
-
-  // Faz o login do usúario
-
-  sql = `
-    CREATE TABLE login (
-      email VARCHAR(255) PRIMARY KEY,
-      senha VARCHAR(255) NOT NULL
+    CREATE TABLE Usuarios(
+      UsuarioID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      Nome VARCHAR(255) NOT NULL,
+      Email VARCHAR(255) NOT NULL,
+      Senha VARCHAR(255) NOT NULL,
+      Perfil TEXT CHECK(Perfil IN ('Artista', 'Gestor Cultural', 'Público Geral')) NOT NULL,
+      DataCadastro DATETIME,
+      Ativo BOOLEAN
     )
   `
 
   await banco.run(sql)
 
-  // Criar o registro do feedback
   sql = `
-    CREATE TABLE feedback (
-      id INTEGER AUTO INTEGER PRIMARY KEY,
-      projeto VARCHAR(255) NOT NULL,
-      artista VARCHAR(255) NOT NULL,
-      comentario VARCHAR(255) NOT NULL
+    CREATE TABLE PerfisArtistas(
+      PerfilArtistaID INT AUTO INCREMENTE PRIMARY KEY NOT NULL,
+      UsuarioID INT NOT NULL,
+      AreaAtuacao VARCHAR(255) NOT NULL,
+      Biografia TEXT,
+      LinkPortfolio VARCHAR(255),
+      FotoPerfil VARCHAR(255),
+      FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+    )
+  `
+
+  await banco.run(sql)
+
+  // Criar a tabela de editais
+  sql = `
+    CREATE TABLE Editais (
+      EditalID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      Titulo VARCHAR(255) NOT NULL,
+      Descricao TEXT NOT NULL,
+      CategoriaArtistica VARCHAR(255) NOT NULL,
+      PrazoInscricao DATE NOT NULL,
+      DetalhesFinanciamento VARCHAR(255),
+      CritériosSelecao TEXT NOT NULL,
+      ProcessoInscricao TEXT NOT NULL,
+      Organizador VARCHAR(255) NOT NULL,
+      DataPublicacao DATE NOT NULL
+    )
+  `
+
+  await banco.run(sql)
+
+  // Criar a tabela de inscrisões em editais
+  sql = `
+    CREATE TABLE InscricoesEditais (
+      InscricaoEditalID INT PRIMARY KEY NOT NULL,
+      EditalID INT NOT NULL,
+      UsuarioID INT NOT NULL,
+      Status TEXT CHECK(Status IN ('Inscrito', 'Avaliado', 'Aprovado', 'Rejeitado')) NOT NULL,
+      DataInscricao DATETIME NOT NULL,
+      Feedback TEXT,
+      CONSTRAINT fk_edital FOREIGN KEY (EditalID) REFERENCES editais(EditalID),
+      CONSTRAINT fk_usuario FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+    )
+  `
+
+  await banco.run(sql)
+
+  // Criar a tabela de Eventos
+  sql = `
+    CREATE TABLE Eventos (
+      EventoID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      NomeEvento VARCHAR(255) NOT NULL,
+      Descricao TEXT NOT NULL,
+      DataHora DATETIME NOT NULL,
+      Localizacao VARCHAR(255) NOT NULL,
+      Organizador VARCHAR(255) NOT NULL,
+      InfoIngresso VARCHAR(255),
+      ImagemCartaz VARCHAR(255),
+      DataPublicacao DATE NOT NULL
+    )
+  `
+
+  await banco.run(sql)
+
+  sql = `
+    CREATE TABLE InscricoesEventos(
+      InscricaoEventoID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      EventoID INT NOT NULL,
+      UsuarioID INT NOT NULL,
+      DataInscricao DATETIME NOT NULL,
+      CONSTRAINT fk_evento FOREIGN KEY (EventoID) REFERENCES Evento(EventoID),
+      CONSTRAINT fk_usuario FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+    )
+  `
+
+  await banco.run(sql)
+
+  sql = `
+    CREATE TABLE ProjetosColaborativos(
+      ProjetoID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      Titulo VARCHAR(255) NOT NULL,
+      Descricao TEXT NOT NULL,
+      AreaAtuacaoNecessaria VARCHAR(255) NOT NULL,
+      Localizacao VARCHAR(255),
+      TipoColaboracao VARCHAR(255) NOT NULL,
+      CriadorID INT NOT NULL,
+      DataCriacao DATE NOT NULL,
+      FOREIGN KEY (CriadorID) REFERENCES Usuarios(CriadorID)
+    )
+  `
+
+  await banco.run(sql)
+
+  sql = `
+    CREATE TABLE ParticipacoesProjetos(
+      ParticipacaoID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      ProjetoID INT NOT NULL,
+      ParticipanteID INT NOT NULL,
+      DataParticipacao DATE NOT NULL,
+      CONSTRAINT fk_projeto FOREIGN KEY (ProjetoID) REFERENCES ProjetosColaborativos(ProjetoID),
+      CONSTRAINT fk_usuario FOREIGN KEY (ParticipanteID) REFERENCES Usuarios(UsuarioID)
+    )
+  `
+
+  await banco.run(sql)
+
+  sql = `
+    CREATE TABLE FeedbacksProjetos(
+      FeedbackID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      ProjetoID INT NOT NULL,
+      AutorID INT NOT NULL,
+      Comentario TEXT,
+      Nota INT,
+      DataFeedback DATE NOT NULL,
+      CONSTRAINT fk_projeto FOREIGN KEY (ProjetoID) REFERENCES ProjetosColaborativos(ProjetoID),
+      CONSTRAINT fk_usuario FOREIGN KEY (AutorID) REFERENCES Usuarios(UsuarioID)
+    )
+  `
+
+  await banco.run(sql)
+
+  sql = `
+    CREATE TABLE DuvidasSugestoesErros(
+      DuvidaSugestaoErroID INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+      UsuarioID INT,
+      Tipo TEXT CHECK(Tipo IN ('Dúvida', 'Sugestão', 'Erro')) NOT NULL,
+      Descricao TEXT NOT NULL,
+      ArquivoAnexo VARCHAR(255) NOT NULL,
+      DataEnvio DATETIME NOT NULL,
+      FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
     )
   `
 
