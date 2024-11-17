a861d8f05770695b69a0e6970914667b00a04946
Marcelo Leonardo <marcelo.leonardo@academico.ifpb.edu.br>
2024-11-17 17:17:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pagínas/login';
+import ProtectedComponent from './components/ProtectedComponent'; 
+
+function RotaProtegida({ children }) {
+    const token = localStorage.getItem('token');
+    return token ? children : <Navigate to="/login" />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/rota-protegida" element={<RotaProtegida><ProtectedComponent /></RotaProtegida>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
