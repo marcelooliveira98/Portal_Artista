0a98bbb7b65048e4d5fcd7e39564250219ae5a39
Marcelo Leonardo <marcelo.leonardo@academico.ifpb.edu.br>
2024-11-18 09:19:03 -0300

    Update login.js

--- src/pagínas/login.js
+++ src/pagínas/login.js
@@ -1,75 +1,98 @@
-import styles from "../Assets/css/login.module.css"
-import { Link } from "react-router-dom"
-import { useState } from "react"
-import imagem from "../Assets/img/img login/Mensagem.png"
+import styles from "../Assets/css/login.module.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import imagem from "../Assets/img/img login/Mensagem.png";
 
-export default function Login()
-{
-
+export default function Login() {
     const [dados, setDados] = useState({
         email: "",
         senha: ""
-    })
+    });
 
-    function pegar_dados(e)
-    {
+    function pegar_dados(e) {
         switch (e.target.id) {
             case "email":
                 setDados({
-                    email: e.target.value,
-                    senha: dados.senha
-                })
-                break
-
+                    ...dados,
+                    email: e.target.value
+                });
+                break;
             case "senha":
                 setDados({
-                    email: dados.email,
+                    ...dados,
                     senha: e.target.value
-                })
-                break
+                });
+                break;
+            default:
+                break;
         }
     }
 
-    async function login(dados2)
-    {   
-        await fetch(`http://localhost:3001/validar?email=${dados2.email}&senha=${dados2.senha}`).then(dados => dados.json()).then(async auteticacao => {
-            if (auteticacao == false) {
-                alert("Email ou senha estão incorretos")
-    
+    async function login(dados2) {
+        try {
+            const response = await fetch(`http://localhost:3001/validar?email=${dados2.email}&senha=${dados2.senha}`);
+            const autenticacao = await response.json();
+            
+            if (!autenticacao.token) {
+                alert("Email ou senha estão incorretos");
             } else {
-                await fetch(`http://localhost:3001/validar?email=${dados2.email}&senha=${dados2.senha}`).then(dados => dados.json()).then(dados => {
-                    localStorage.setItem("usuario_id", dados.UsuarioID)
-                })
-    
-                window.location.href = "/portal_artistas"
+                // Armazena o token JWT no localStorage
+                localStorage.setItem("token", autenticacao.token);
+                window.location.href = "/portal_artistas"; // Redireciona para a página protegida
             }
-
-        })
+        } catch (error) {
+            console.error("Erro ao fazer login:", error);
+            alert("Ocorreu um erro. Tente novamente mais tarde.");
+        }
     }
 
-    return(
+    return (
         <div className={styles.corpo}>
             <div className={styles.parte1}>
                 <h1 className={styles.titulo}>Bem-Vindo</h1>
                 <p className={styles.texto}>Nosso Site? Registre-se</p>
 
                 <form className={styles.box1}>
-                    <input className={styles.campos_form} id="email" onChange={(e) => pegar_dados(e)} placeholder="Email:" type="text"/>
-                    <input className={styles.campos_form} id="senha" onChange={(e) => pegar_dados(e)} placeholder="Senha:" type="password"/>
+                    <input
+                        className={styles.campos_form}
+                        id="email"
+                        onChange={(e) => pegar_dados(e)}
+                        placeholder="Email:"
+                        type="text"
+                    />
+                    <input
+                        className={styles.campos_form}
+                        id="senha"
+                        onChange={(e) => pegar_dados(e)}
+                        placeholder="Senha:"
+                        type="password"
+                    />
+                    <input
+                        className={`${styles.campos_form} ${styles.botao_enviar}`}
+                        onClick={() => login(dados)}
+                        type="button"
+                        value="Entrar"
+                    />
 
-                    <input className={`${styles.campos_form} ${styles.botao_enviar}`} onClick={() => login(dados)} type="button" value="Entrar"/>
+                    <Link to="/recuperar_senha">
+                        <p className={styles.esquece_senha}>Esqueceu a Senha?</p>
+                    </Link>
 
-                    <Link to="/recuperar_senha"><p className={styles.esquece_senha}>Esqueceu a Senhar?</p></Link>
+                    <hr className={styles.divisao} />
 
-                    <hr className={styles.divisao}/>
-
-                    <Link to="/criar_conta"><input className={`${styles.campos_form} ${styles.botao_azul}`} type="button" value="Criar uma contar"/></Link>
+                    <Link to="/criar_conta">
+                        <input
+                            className={`${styles.campos_form} ${styles.botao_azul}`}
+                            type="button"
+                            value="Criar uma conta"
+                        />
+                    </Link>
                 </form>
             </div>
-            
+
             <div className={styles.container_img}>
-                <img className={styles.mensagem} src={imagem} alt="Desperte sua criatividade"/>
+                <img className={styles.mensagem} src={imagem} alt="Desperte sua criatividade" />
             </div>
         </div>
-    )
+    );
 }
